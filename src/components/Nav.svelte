955bdb2146e301fbf1e4ad121d32166b41995f9d
tjolaa <anton_1891@hotmail.com>
2022-07-27 13:55:36 +0200

    google maps

---
+++ src/components/Nav.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+import Hamburger from "./icons/Hamburger.svelte";
+
+
+ export let sidebar: boolean = false;
+</script>
+
+<header class="flex justify-between bg-gray-200 p-2 items-center text-gray-600 border-b-2">
+	<nav class="flex">
+		<Hamburger bind:open={sidebar}/>
+	</nav>
+	
+</header>
